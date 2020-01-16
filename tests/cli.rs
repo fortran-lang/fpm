7b53dd48556cf792359c030fcdb01693e1db45b4
Ondřej Čertík <ondrej@certik.us>
2020-01-15 22:44:55 -0700

    Add our own implementation of success()

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,32 +1,42 @@
 use std::process::Command;  // Run programs
 use assert_cmd::prelude::*; // Add methods on commands
 use predicates::prelude::*; // Used for writing assertions
+use assert_cmd::assert::Assert;
+
+// Our own function, so that we can better debug the failure
+fn success(self0: Assert) -> Assert {
+    if !self0.get_output().status.success() {
+        let actual_code = self0.get_output().status.code().unwrap_or_else(|| {
+            panic!("INTERRUPTED")
+        });
+        panic!("Non zero exit code: {}", actual_code);
+    }
+    self0
+}
 
 #[test]
 fn test_help() {
     let mut cmd = Command::cargo_bin("fpm").unwrap();
     cmd.arg("--help");
-    cmd.assert()
-        .success()
+    success(cmd.assert())
         .stdout(
             predicate::str::contains("--help       Prints help information"));
 }
+
 
 #[test]
 fn test_1() {
     let mut build = Command::cargo_bin("fpm").unwrap();
     build.arg("build")
         .current_dir("tests/1");
-    build.assert()
-        .success()
+    success(build.assert())
         .stdout(predicate::str::contains("Built target p1")
                 .and(predicate::str::contains("TEST1 OK").not()));
 
     let mut run = Command::cargo_bin("fpm").unwrap();
     run.arg("run")
         .current_dir("tests/1");
-    run.assert()
-        .success()
+    success(run.assert())
         .stdout(predicate::str::contains("TEST1 OK"));
 }
 
@@ -35,15 +45,13 @@ fn test_2() {
     let mut build = Command::cargo_bin("fpm").unwrap();
     build.arg("build")
         .current_dir("tests/2");
-    build.assert()
-        .success()
+    success(build.assert())
         .stdout(predicate::str::contains("Built target p1")
                 .and(predicate::str::contains("TEST2 OK").not()));
 
     let mut run = Command::cargo_bin("fpm").unwrap();
     run.arg("run")
         .current_dir("tests/2");
-    run.assert()
-        .success()
+    success(run.assert())
         .stdout(predicate::str::contains("TEST2 OK"));
 }
