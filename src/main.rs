892885f0076ef686cec6addf34f407a1cc63bd74
Ondřej Čertík <ondrej@certik.us>
2020-02-01 13:41:06 -0700

    Implement common_prefix

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,54 @@ fn collect_source_files() -> Vec<String> {
     files
 }
 
+#[test]
+fn test_common_prefix() {
+    let paths = [
+        Path::new("/home/user1/tmp/coverage/test"),
+        Path::new("/home/user1/tmp/covert/operator"),
+        Path::new("/home/user1/tmp/coven/members"),
+    ];
+    let p = common_prefix(&paths).unwrap();
+    assert_eq!(p.to_str().unwrap(), "/home/user1/tmp")
+}
+
+fn common_prefix<I, P>(paths: I) -> Option<PathBuf>
+where
+    I: IntoIterator<Item = P>,
+    P: AsRef<Path>,
+{
+    let mut iter = paths.into_iter();
+    let mut ret = iter.next()?.as_ref().to_path_buf();
+    for path in iter {
+        if let Some(r) = common(ret, path.as_ref()) {
+            ret = r;
+        } else {
+            return None;
+        }
+    }
+    Some(ret)
+}
+ 
+fn common<A: AsRef<Path>, B: AsRef<Path>>(a: A, b: B) -> Option<PathBuf> {
+    let a = a.as_ref().components();
+    let b = b.as_ref().components();
+    let mut ret = PathBuf::new();
+    let mut found = false;
+    for (one, two) in a.zip(b) {
+        if one == two {
+            ret.push(one);
+            found = true;
+        } else {
+            break;
+        }
+    }
+    if found {
+        Some(ret)
+    } else {
+        None
+    }
+}
+
 fn build(target_dir: &Path) {
     let target = PathBuf::from(target_dir);
     println!("TARGET_DIR: {}", target_dir.to_str().unwrap());
