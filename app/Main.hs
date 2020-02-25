d9cd40f9d84e104207f8ef34c6d5a4f9d1b3af4b
Brad Richardson <brichardson@structint.com>
2020-02-25 11:18:40 -0800

    Add bare minimum of command line parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,43 @@
 module Main where
 
-import Lib
+import           Options.Applicative
+
+newtype Arguments = Arguments { command' :: Command }
+
+data Command = Run | Test | Build
 
 main :: IO ()
-main = someFunc
+main = do
+    args <- getArguments
+    run args
+
+run :: Arguments -> IO ()
+run args = case command' args of
+    Run   -> putStrLn "Run"
+    Test  -> putStrLn "Test"
+    Build -> putStrLn "Build"
+
+getArguments :: IO Arguments
+getArguments = execParser
+    (info
+        (arguments <**> helper)
+        (fullDesc <> progDesc "Work with Fortran projects" <> header
+            "fpm - A Fortran package manager and build system"
+        )
+    )
+
+arguments :: Parser Arguments
+arguments = subparser
+    (  command "run"   (info runArguments (progDesc "Run the executable"))
+    <> command "test"  (info testArguments (progDesc "Run the tests"))
+    <> command "build" (info buildArguments (progDesc "Build the executable"))
+    )
+
+runArguments :: Parser Arguments
+runArguments = pure $ Arguments Run
+
+testArguments :: Parser Arguments
+testArguments = pure $ Arguments Test
+
+buildArguments :: Parser Arguments
+buildArguments = pure $ Arguments Build
