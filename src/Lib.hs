6a79e31d9b6eaffdca9ce0a5fe9471d60c20530f
Ondřej Čertík <ondrej@certik.us>
2020-02-25 14:44:04 -0800

    Compile using Fortran

--- src/Lib.hs
+++ src/Lib.hs
@@ -9,5 +9,6 @@ import System.Process (callCommand)
 someFunc :: IO ()
 someFunc = do
     putStrLn "someFunc"
-    a <- callCommand "ls -l"
+    a <- callCommand "gfortran test/test1.f90 -o test1"
+    a <- callCommand "./test1"
     return ()
