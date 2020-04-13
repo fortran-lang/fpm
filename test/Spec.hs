e4829d620dd9c5261880cee92f49da43f2a8ed20
Brad Richardson <brichardson@structint.com>
2020-04-13 18:57:17 -0500

    Move test code out of src folder

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,20 @@
-import Lib (someFunc, someFunc2)
+import           Development.Shake.FilePath     ( (</>) )
+import           System.Process                 ( callCommand )
 
 main :: IO ()
 main = do
-    someFunc
-    someFunc2
+  someFunc
+  someFunc2
+
+someFunc :: IO ()
+someFunc = do
+  putStrLn "someFunc"
+  a <- callCommand "gfortran test/test1.f90 -o test1"
+  a <- callCommand $ "." </> "test1"
+  return ()
+
+someFunc2 :: IO ()
+someFunc2 = do
+  putStrLn "example"
+  a <- callCommand "cd example_project && stack run -- build"
+  return ()
