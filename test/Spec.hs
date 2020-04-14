32af0dea4bfa1aa30525048ddcd91f74727e2a52
Brad Richardson <brichardson@structint.com>
2020-04-13 19:10:39 -0500

    Simplify the tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,20 +1,11 @@
 import           Development.Shake.FilePath     ( (</>) )
+import           System.Directory               ( withCurrentDirectory )
 import           System.Process                 ( callCommand )
 
 main :: IO ()
 main = do
-  someFunc
-  someFunc2
+  testExampleProject
 
-someFunc :: IO ()
-someFunc = do
-  putStrLn "someFunc"
-  a <- callCommand "gfortran test/test1.f90 -o test1"
-  a <- callCommand $ "." </> "test1"
-  return ()
-
-someFunc2 :: IO ()
-someFunc2 = do
-  putStrLn "example"
-  a <- callCommand "cd example_project && stack run -- build"
-  return ()
+testExampleProject :: IO ()
+testExampleProject =
+  withCurrentDirectory "example_project" $ callCommand "stack run -- build"
--- test/test1.f90
+++
@@ -1,8 +0,0 @@
-program test1
-implicit none
-integer :: i
-i = 5+5
-print *, i
-print *, "Fortran test code executed"
-print *, "TESTS PASSED"
-end program
