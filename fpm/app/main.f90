86e3d025dfe57c1129b0667cf59eae364089324b
Ondřej Čertík <ondrej@certik.us>
2020-07-21 14:03:06 -0600

    Implement initial `fpm build`
    Right now it only knows how to build itself.

--- fpm/app/main.f90
+++ fpm/app/main.f90
@@ -1,7 +1,20 @@
 program main
-  use fpm, only: say_hello
+use fpm, only: print_help, cmd_build
+implicit none
+character(100) :: cmdarg
 
-  implicit none
-
-  call say_hello
+if (command_argument_count() == 0) then
+    call print_help()
+else if (command_argument_count() == 1) then
+    call get_command_argument(1, cmdarg)
+    if (cmdarg == "build") then
+        call cmd_build()
+    else
+        print *, "Unknown command: ", cmdarg
+        error stop
+    end if
+else
+    print *, "Too many arguments"
+    error stop
+end if
 end program main
