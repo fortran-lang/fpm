3bb8b39eccafc5acb0ee08a201c09aa0e828ce28
Ondřej Čertík <ondrej@certik.us>
2020-07-21 16:19:01 -0600

    Extract the CI tests into a Bash and Batch files

---
+++ ci/run_tests.bat
@@ -0,0 +1,19 @@
+@echo on
+
+cd fpm
+if errorlevel 1 exit 1
+
+fpm build
+if errorlevel 1 exit 1
+
+fpm run
+if errorlevel 1 exit 1
+
+fpm run --args build
+if errorlevel 1 exit 1
+
+cd ..\test\example_packages\hello_world
+if errorlevel 1 exit 1
+
+..\..\..\fpm\fpm build
+if errorlevel 1 exit 1
---
+++ ci/run_tests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -ex
+
+cd fpm
+fpm build
+fpm run
+fpm run --args build
+cd ../test/example_packages/hello_world
+../../../fpm/fpm build
