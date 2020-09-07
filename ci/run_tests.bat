5500927beb1a81a39cf3f155a66b8fe5cad5b769
LKedward <laurence.kedward@bristol.ac.uk>
2020-09-07 10:10:27 +0100

    Update: test scripts for fortran fpm
    Fortran fpm can now build 'hello_complex' example

--- ci/run_tests.bat
+++ ci/run_tests.bat
@@ -21,5 +21,24 @@ if errorlevel 1 exit 1
 ..\..\..\fpm\build\gfortran_debug\app\fpm build
 if errorlevel 1 exit 1
 
-.\build\gfortran_debug\hello_world
+.\build\gfortran_debug\app\hello_world
+if errorlevel 1 exit 1
+
+
+cd ..\hello_complex
+if errorlevel 1 exit 1
+
+..\..\..\fpm\build\gfortran_debug\app\fpm build
+if errorlevel 1 exit 1
+
+.\build\gfortran_debug\app\say_Hello
+if errorlevel 1 exit 1
+
+.\build\gfortran_debug\app\say_goodbye
+if errorlevel 1 exit 1
+
+.\build\gfortran_debug\test\greet_test
+if errorlevel 1 exit 1
+
+.\build\gfortran_debug\test\farewell_test
 if errorlevel 1 exit 1
--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -7,6 +7,14 @@ fpm build
 fpm run
 fpm test
 build/gfortran_debug/app/fpm
+
 cd ../test/example_packages/hello_world
 ../../../fpm/build/gfortran_debug/app/fpm build
-./build/gfortran_debug/hello_world
+./build/gfortran_debug/app/hello_world
+
+cd ../hello_complex
+../../../fpm/build/gfortran_debug/app/fpm build
+./build/gfortran_debug/app/say_Hello
+./build/gfortran_debug/app/say_goodbye
+./build/gfortran_debug/test/greet_test
+./build/gfortran_debug/test/farewell_test
