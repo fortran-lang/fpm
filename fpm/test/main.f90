f843ec6ed960ffffb085da57d6ce19e084c76fb8
LKedward <laurence.kedward@bristol.ac.uk>
2020-09-12 14:10:53 +0100

    Add: unit tests for fpm_sources parsing routines

--- fpm/test/main.f90
+++ fpm/test/main.f90
@@ -4,6 +4,7 @@ program fpm_testing
     use testsuite, only : run_testsuite
     use test_toml, only : collect_toml
     use test_manifest, only : collect_manifest
+    use test_source_parsing, only : collect_source_parsing
     implicit none
     integer :: stat
     character(len=*), parameter :: fmt = '("#", *(1x, a))'
@@ -18,6 +19,14 @@ program fpm_testing
 
     write(error_unit, fmt) "Testing:", "fpm_manifest"
     call run_testsuite(collect_manifest, error_unit, stat)
+
+    if (stat > 0) then
+        write(error_unit, '(i0, 1x, a)') stat, "tests failed!"
+        error stop 1
+    end if
+
+    write(error_unit, fmt) "Testing:", "fpm_sources (parsing)"
+    call run_testsuite(collect_source_parsing, error_unit, stat)
 
     if (stat > 0) then
         write(error_unit, '(i0, 1x, a)') stat, "tests failed!"
