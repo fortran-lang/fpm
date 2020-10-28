6f7ae5b6f1b45a4d8c12dfca2d188f316efe993b
Brad Richardson <everythingfunctional@protonmail.com>
2020-10-28 10:17:03 -0500

    Fix Linux/MacOS CI script

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -1,55 +1,70 @@
 #!/bin/bash
 
+get_abs_filename() {
+  # $1 : relative filename
+  filename=$1
+  parentdir=$(dirname "${filename}")
+
+  if [ -d "${filename}" ]; then
+      echo "$(cd "${filename}" && pwd)"
+  elif [ -d "${parentdir}" ]; then
+    echo "$(cd "${parentdir}" && pwd)/$(basename "${filename}")"
+  fi
+}
+
 set -ex
 
 cd fpm
 fpm build
 fpm run
 fpm test
-build/gfortran_debug/app/fpm
+
+f_fpm_path="$(get_abs_filename $(find build -regex 'build/.*/app/fpm'))"
+
+"${f_fpm_path}"
 
 cd ../example_packages/hello_world
-../../fpm/build/gfortran_debug/app/fpm build
+"${f_fpm_path}" build
 ./build/gfortran_debug/app/hello_world
 
 cd ../hello_fpm
-../../fpm/build/gfortran_debug/app/fpm build
+"${f_fpm_path}" build
 ./build/gfortran_debug/app/hello_fpm
 
 cd ../circular_test
-../../fpm/build/gfortran_debug/app/fpm build
+"${f_fpm_path}" build
 
 cd ../circular_example
-../../fpm/build/gfortran_debug/app/fpm build
+"${f_fpm_path}" build
 
 cd ../hello_complex
-../../fpm/build/gfortran_debug/app/fpm build
+"${f_fpm_path}" build
 ./build/gfortran_debug/app/say_Hello
 ./build/gfortran_debug/app/say_goodbye
 ./build/gfortran_debug/test/greet_test
 ./build/gfortran_debug/test/farewell_test
 
 cd ../hello_complex_2
-../../fpm/build/gfortran_debug/app/fpm build
+"${f_fpm_path}" build
 ./build/gfortran_debug/app/say_hello_world
 ./build/gfortran_debug/app/say_goodbye
 ./build/gfortran_debug/test/greet_test
 ./build/gfortran_debug/test/farewell_test
 
 cd ../auto_discovery_off
-../../fpm/build/gfortran_debug/app/fpm build
+"${f_fpm_path}" build
 ./build/gfortran_debug/app/auto_discovery_off
 ./build/gfortran_debug/test/my_test
 test ! -x ./build/gfortran_debug/app/unused
 test ! -x ./build/gfortran_debug/test/unused_test
 
 cd ../with_c
-../../fpm/build/gfortran_debug/app/fpm build
+"${f_fpm_path}" build
 ./build/gfortran_debug/app/with_c
 
 cd ../submodules
-../../fpm/build/gfortran_debug/app/fpm build
+"${f_fpm_path}" build
 
 cd ../program_with_module
-../../fpm/build/gfortran_debug/app/fpm build
+"${f_fpm_path}" build
 ./build/gfortran_debug/app/Program_with_module
