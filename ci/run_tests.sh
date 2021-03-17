bef08ccdf7053410825ade463221007ef7186062
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2021-03-17 22:34:04 +0100

    Don't use explicit paths to targets in CI tests

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -30,12 +30,12 @@ rm -rf ./*/build
 cd hello_world
 
 "${f_fpm_path}" build
-./build/gfortran_debug/app/hello_world
+"${f_fpm_path}" run --target hello_world
 "${f_fpm_path}" run
 
 cd ../hello_fpm
 "${f_fpm_path}" build
-./build/gfortran_debug/app/hello_fpm
+"${f_fpm_path}" run --target hello_fpm
 
 cd ../circular_test
 "${f_fpm_path}" build
@@ -46,48 +46,48 @@ cd ../circular_example
 cd ../hello_complex
 "${f_fpm_path}" build
 "${f_fpm_path}" test
-./build/gfortran_debug/app/say_Hello
-./build/gfortran_debug/app/say_goodbye
-./build/gfortran_debug/test/greet_test
-./build/gfortran_debug/test/farewell_test
+"${f_fpm_path}" run --target say_Hello
+"${f_fpm_path}" run --target say_goodbye
+"${f_fpm_path}" test --target greet_test
+"${f_fpm_path}" test --target farewell_test
 
 cd ../hello_complex_2
 "${f_fpm_path}" build
-./build/gfortran_debug/app/say_hello_world
-./build/gfortran_debug/app/say_goodbye
-./build/gfortran_debug/test/greet_test
-./build/gfortran_debug/test/farewell_test
+"${f_fpm_path}" run --target say_hello_world
+"${f_fpm_path}" run --target say_goodbye
+"${f_fpm_path}" test --target greet_test
+"${f_fpm_path}" test --target farewell_test
 
 cd ../with_examples
 "${f_fpm_path}" build
-./build/gfortran_debug/example/demo-prog
-./build/gfortran_debug/app/demo-prog
+"${f_fpm_path}" run --example --target demo-prog
+"${f_fpm_path}" run --target demo-prog
 
 cd ../auto_discovery_off
 "${f_fpm_path}" build
-./build/gfortran_debug/app/auto_discovery_off
-./build/gfortran_debug/test/my_test
-test ! -x ./build/gfortran_debug/app/unused
-test ! -x ./build/gfortran_debug/test/unused_test
+"${f_fpm_path}" run --target auto_discovery_off
+"${f_fpm_path}" test --target my_test
+test ! -x ./build/gfortran_*/app/unused
+test ! -x ./build/gfortran_*/test/unused_test
 
 cd ../with_c
 "${f_fpm_path}" build
-./build/gfortran_debug/app/with_c
+"${f_fpm_path}" run --target with_c
 
 cd ../submodules
 "${f_fpm_path}" build
 
 cd ../program_with_module
 "${f_fpm_path}" build
-./build/gfortran_debug/app/Program_with_module
+"${f_fpm_path}" run --target Program_with_module
 
 cd ../link_external
 "${f_fpm_path}" build
-./build/gfortran_debug/app/link_external
+"${f_fpm_path}" run --target link_external
 
 cd ../link_executable
 "${f_fpm_path}" build
-./build/gfortran_debug/app/gomp_test
+"${f_fpm_path}" run --target gomp_test
 
 # Cleanup
 rm -rf ./*/build
