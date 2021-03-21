7e826ec7df5644da27ac40c415f07615bcf2bd12
John S. Urban <urbanjost@comcast.net>
2021-03-21 11:23:05 -0400

    correct join for null input
    If the input string is null join would overindex by using element
    one when adding the left and right strings. This corrects that.

--- fpm/src/fpm_strings.f90
+++ fpm/src/fpm_strings.f90
@@ -524,10 +524,14 @@ character(len=:),allocatable         :: right_local
          string=string//left_local//str(i)//right_local//sep_local
       endif
    enddo
-   if(trm_local)then
-      string=string//left_local//trim(str(i))//right_local
+   if(size(str).lt.i)then ! if str is zero-sized i will be 1, not zero
+      string=string//left_local//right_local
    else
-      string=string//left_local//str(i)//right_local
+      if(trm_local)then
+         string=string//left_local//trim(str(i))//right_local
+      else
+         string=string//left_local//str(i)//right_local
+      endif
    endif
    if(present(start))string=start//string
    if(present(end))string=string//end
