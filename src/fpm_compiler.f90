975e9cbe2506ac46e607080710e0314dabb07dd5
LKedward <laurence.kedward@bristol.ac.uk>
2021-04-17 12:47:50 +0100

    Remove coarray flag from intel debug settings

--- src/fpm_compiler.f90
+++ src/fpm_compiler.f90
@@ -239,7 +239,6 @@ subroutine get_debug_compile_flags(id, flags)
             & -g&
             & -assume byterecl&
             & -traceback&
-            & -coarray=single&
             &'
     case(id_intel_classic_mac)
         flags = '&
@@ -260,7 +259,6 @@ subroutine get_debug_compile_flags(id, flags)
             & /Z7&
             & /assume:byterecl&
             & /traceback&
-            & /Qcoarray:single&
             &'
     case(id_intel_llvm_nix, id_intel_llvm_unknown)
         flags = '&
@@ -271,7 +269,6 @@ subroutine get_debug_compile_flags(id, flags)
             & -g&
             & -assume byterecl&
             & -traceback&
-            & -coarray=single&
             &'
     case(id_intel_llvm_windows)
         flags = '&
@@ -281,7 +278,6 @@ subroutine get_debug_compile_flags(id, flags)
             & /Od&
             & /Z7&
             & /assume:byterecl&
-            & /Qcoarray:single&
             &'
     case(id_nag)
         flags = '&
