63288a62ceeb20c53ba656b89c0f7c709ad9dbaa
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2021-06-23 08:56:32 +0200

    Add meaningful error message to example package
    Co-authored-by: Milan Curcic <caomaco@gmail.com>

--- example_packages/version_file/app/main.f90
+++ example_packages/version_file/app/main.f90
@@ -1,10 +1,10 @@
 program stub
   implicit none
-  logical :: exist
+  logical :: exists
   integer :: unit
   character(len=100) :: line
-  inquire(file="VERSION", exist=exist)
-  if (.not.exist) error stop
+  inquire(file="VERSION", exist=exists)
+  if (.not.exists) error stop "File VERSION does not exist."
   open(file="VERSION", newunit=unit)
   read(unit, '(a)') line
   close(unit)
