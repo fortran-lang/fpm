97c802f8e69c1f374fbba7e0fa6029775fb3eaf1
urbanjost <urbanjost@comcast.net>
2021-06-28 08:06:41 -0400

    generic labels

--- src/fpm_backend.f90
+++ src/fpm_backend.f90
@@ -268,20 +268,20 @@ subroutine build_target(model,target,stat)
 
     case (FPM_TARGET_OBJECT)
         if(.not.CONFIG_VERBOSE) write(*,('(a)')) &
-                esc('<B><bo><w>'//model%fortran_compiler//':</B></w><bo>'//target%source%file_name)
+                esc('<B><bo><w>compile:')//target%source%file_name
         call run(model%fortran_compiler//" -c " // target%source%file_name // target%compile_flags &
               // " -o " // target%output_file, echo=CONFIG_VERBOSE, exitstat=stat)
 
     case (FPM_TARGET_C_OBJECT)
         if(.not.CONFIG_VERBOSE) write(*,('(a)')) &
-                esc('<B><bo><w>'//model%fortran_compiler//':</B></w><bo>'//target%source%file_name)
+                esc('<B><bo><w>compile:')//target%source%file_name
         call run(model%c_compiler//" -c " // target%source%file_name // target%compile_flags &
                 // " -o " // target%output_file, echo=CONFIG_VERBOSE, exitstat=stat)
 
     case (FPM_TARGET_EXECUTABLE)
 
         if(.not.CONFIG_VERBOSE) write(*,('(a)')) &
-                esc('<G><bo><w>'//model%fortran_compiler//':</B></w><bo>'//target%output_file)
+                esc('<G><bo><w>load   :')//target%output_file
         call run(model%fortran_compiler// " " // target%compile_flags &
               //" "//target%link_flags// " -o " // target%output_file, echo=CONFIG_VERBOSE, exitstat=stat)
 
@@ -291,13 +291,13 @@ subroutine build_target(model,target,stat)
         case (OS_WINDOWS)
             call write_response_file(target%output_file//".resp" ,target%link_objects)
             if(.not.CONFIG_VERBOSE) write(*,('(a)')) &
-                    esc('<G><bo><w>'//model%archiver//':</G></w><bo>'//target%output_file)
+                    esc('<G><bo><w>archive:')//target%output_file
             call run(model%archiver // target%output_file // " @" // target%output_file//".resp", &
                      echo=CONFIG_VERBOSE, exitstat=stat)
 
         case default
             if(.not.CONFIG_VERBOSE) write(*,('(a)')) &
-                    esc('<G><bo><w>'//model%archiver//':</G></w><bo>'//string_cat(target%link_objects," "))
+                    esc('<G><bo><w>archive:')//string_cat(target%link_objects," ")
             call run(model%archiver // target%output_file // " " // string_cat(target%link_objects," "), &
                      echo=CONFIG_VERBOSE, exitstat=stat)
 
