679bc5d7c25f8160375e02f53f04caa8ffcaacbf
Andre Smit <freevryheid@gmail.com>
2022-01-20 12:05:45 -0600

    Update src/fpm/cmd/new.f90
    Co-authored-by: Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>

--- src/fpm/cmd/new.f90
+++ src/fpm/cmd/new.f90
@@ -590,7 +590,7 @@ end function default_user
 function git_user(what) result(user)
   use fpm_filesystem, only : get_temp_filename, getline
   implicit none
-  character(len=5), intent(in) :: what
+  character(len=*), intent(in) :: what
   character(len=:), allocatable :: user
   character(len=:), allocatable :: temp_user, iomsg
   integer :: stat, unit
