0235f3f3ee5718d0527e39955555ed06d4b57219
Laurence Kedward <laurence.kedward@bristol.ac.uk>
2022-08-30 13:05:44 +0100

    Fix: int type mismatch in cpp example package.

--- example_packages/cpp_files/test/check.f90
+++ example_packages/cpp_files/test/check.f90
@@ -1,11 +1,12 @@
 program check
+  use iso_c_binding, only: c_size_t
   use cpp_files
   implicit none
 
   integer :: i, max_element
   integer, parameter :: array(*) = [(i,i=-50,10)]
   
-  max_element = intvec_maxval(array,size(array,1))
+  max_element = intvec_maxval(array,size(array,1,c_size_t))
 
   if (max_element == maxval(array)) then
     write(*,*) ' PASSED: Max element is ',max_element
