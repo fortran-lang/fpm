76a4f7ae6c3d315bc80dddbf395d4c952a64ce42
gnikit <giannis.nikiteas@gmail.com>
2022-11-24 16:01:27 +0000

    Deploying to gh-pages from  @ 58c7f76cecccf3062e870fab4e066c8719428ee9 🚀

--- src/install.f90
+++ src/install.f90
@@ -1,162 +1,108 @@
-module fpm_cmd_install
-  use, intrinsic :: iso_fortran_env, only : output_unit
-  use fpm, only : build_model
-  use fpm_backend, only : build_package
-  use fpm_command_line, only : fpm_install_settings
-  use fpm_error, only : error_t, fatal_error, fpm_stop
-  use fpm_filesystem, only : join_path, list_files
-  use fpm_installer, only : installer_t, new_installer
-  use fpm_manifest, only : package_config_t, get_package_data
-  use fpm_model, only : fpm_model_t, FPM_SCOPE_APP
-  use fpm_targets, only: targets_from_sources, build_target_t, &
-                         build_target_ptr, FPM_TARGET_EXECUTABLE, &
-                         filter_library_targets, filter_executable_targets, filter_modules
-  use fpm_strings, only : string_t, resize
+!> Implementation of the installation configuration.
+!>
+!> An install table can currently have the following fields
+!>
+!>```toml
+!>library = bool
+!>```
+module fpm_manifest_install
+  use fpm_error, only : error_t, fatal_error, syntax_error
+  use fpm_toml, only : toml_table, toml_key, toml_stat, get_value
   implicit none
   private
 
-  public :: cmd_install
+  public :: install_config_t, new_install_config
+
+  !> Configuration data for installation
+  type :: install_config_t
+
+    !> Install library with this project
+    logical :: library
+
+  contains
+
+    !> Print information on this instance
+    procedure :: info
+
+  end type install_config_t
 
 contains
 
-  !> Entry point for the fpm-install subcommand
-  subroutine cmd_install(settings)
-    !> Representation of the command line settings
-    type(fpm_install_settings), intent(in) :: settings
-    type(package_config_t) :: package
-    type(error_t), allocatable :: error
-    type(fpm_model_t) :: model
-    type(build_target_ptr), allocatable :: targets(:)
-    type(installer_t) :: installer
-    character(len=:), allocatable :: lib, dir
-    type(string_t), allocatable :: list(:)
-    logical :: installable
+  !> Create a new installation configuration from a TOML data structure
+  subroutine new_install_config(self, table, error)
 
-    call get_package_data(package, "fpm.toml", error, apply_defaults=.true.)
-    call handle_error(error)
+    !> Instance of the install configuration
+    type(install_config_t), intent(out) :: self
 
-    call build_model(model, settings%fpm_build_settings, package, error)
-    call handle_error(error)
+    !> Instance of the TOML data structure
+    type(toml_table), intent(inout) :: table
 
-    call targets_from_sources(targets, model, settings%prune, error)
-    call handle_error(error)
+    !> Error handling
+    type(error_t), allocatable, intent(out) :: error
 
-    installable = (allocated(package%library) .and. package%install%library) &
-      .or. allocated(package%executable)
-    if (.not.installable) then
-      call fatal_error(error, "Project does not contain any installable targets")
-      call handle_error(error)
-    end if
+    call check(table, error)
+    if (allocated(error)) return
 
-    if (settings%list) then
-      call install_info(output_unit, package, model, targets)
-      return
-    end if
+    call get_value(table, "library", self%library, .false.)
 
-    if (.not.settings%no_rebuild) then
-      call build_package(targets,model,verbose=settings%verbose)
-    end if
+  end subroutine new_install_config
 
-    call new_installer(installer, prefix=settings%prefix, &
-      bindir=settings%bindir, libdir=settings%libdir, &
-      includedir=settings%includedir, &
-      verbosity=merge(2, 1, settings%verbose))
 
-    if (allocated(package%library) .and. package%install%library) then
-      call filter_library_targets(targets, list)
+  !> Check local schema for allowed entries
+  subroutine check(table, error)
 
-      if (size(list) > 0) then
-        call installer%install_library(list(1)%s, error)
-        call handle_error(error)
+    !> Instance of the TOML data structure
+    type(toml_table), intent(inout) :: table
 
-        call install_module_files(installer, targets, error)
-        call handle_error(error)
-      end if
-    end if
+    !> Error handling
+    type(error_t), allocatable, intent(out) :: error
 
-    if (allocated(package%executable)) then
-      call install_executables(installer, targets, error)
-      call handle_error(error)
-    end if
+    type(toml_key), allocatable :: list(:)
+    integer :: ikey
 
-  end subroutine cmd_install
+    call table%get_keys(list)
+    if (size(list) < 1) return
 
-  subroutine install_info(unit, package, model, targets)
+    do ikey = 1, size(list)
+      select case(list(ikey)%key)
+      case default
+        call syntax_error(error, "Key "//list(ikey)%key//" is not allowed in install table")
+        exit
+      case("library")
+        continue
+      end select
+    end do
+    if (allocated(error)) return
+
+  end subroutine check
+
+  !> Write information on install configuration instance
+  subroutine info(self, unit, verbosity)
+
+    !> Instance of the build configuration
+    class(install_config_t), intent(in) :: self
+
+    !> Unit for IO
     integer, intent(in) :: unit
-    type(package_config_t), intent(in) :: package
-    type(fpm_model_t), intent(in) :: model
-    type(build_target_ptr), intent(in) :: targets(:)
 
-    integer :: ii, ntargets
-    character(len=:), allocatable :: lib
-    type(string_t), allocatable :: install_target(:), temp(:)
+    !> Verbosity of the printout
+    integer, intent(in), optional :: verbosity
 
-    allocate(install_target(0))
+    integer :: pr
+    character(len=*), parameter :: fmt = '("#", 1x, a, t30, a)'
 
-    call filter_library_targets(targets, temp)
-    install_target = [install_target, temp]
-
-    call filter_executable_targets(targets, FPM_SCOPE_APP, temp)
-    install_target = [install_target, temp]
-
-    ntargets = size(install_target)
-
-    write(unit, '("#", *(1x, g0))') &
-      "total number of installable targets:", ntargets
-    do ii = 1, ntargets
-      write(unit, '("-", *(1x, g0))') install_target(ii)%s
-    end do
-
-  end subroutine install_info
-
-  subroutine install_module_files(installer, targets, error)
-    type(installer_t), intent(inout) :: installer
-    type(build_target_ptr), intent(in) :: targets(:)
-    type(error_t), allocatable, intent(out) :: error
-    type(string_t), allocatable :: modules(:)
-    integer :: ii
-
-    call filter_modules(targets, modules)
-
-    do ii = 1, size(modules)
-      call installer%install_header(modules(ii)%s//".mod", error)
-      if (allocated(error)) exit
-    end do
-    if (allocated(error)) return
-
-  end subroutine install_module_files
-
-  subroutine install_executables(installer, targets, error)
-    type(installer_t), intent(inout) :: installer
-    type(build_target_ptr), intent(in) :: targets(:)
-    type(error_t), allocatable, intent(out) :: error
-    integer :: ii
-
-    do ii = 1, size(targets)
-      if (is_executable_target(targets(ii)%ptr)) then
-        call installer%install_executable(targets(ii)%ptr%output_file, error)
-        if (allocated(error)) exit
-      end if
-    end do
-    if (allocated(error)) return
-
-  end subroutine install_executables
-
-  elemental function is_executable_target(target_ptr) result(is_exe)
-    type(build_target_t), intent(in) :: target_ptr
-    logical :: is_exe
-    is_exe = target_ptr%target_type == FPM_TARGET_EXECUTABLE .and. &
-      allocated(target_ptr%dependencies)
-    if (is_exe) then
-      is_exe = target_ptr%dependencies(1)%ptr%source%unit_scope == FPM_SCOPE_APP
+    if (present(verbosity)) then
+      pr = verbosity
+    else
+      pr = 1
     end if
-  end function is_executable_target
 
-  subroutine handle_error(error)
-    type(error_t), intent(in), optional :: error
-    if (present(error)) then
-      call fpm_stop(1,error%message)
-    end if
-  end subroutine handle_error
+    if (pr < 1) return
 
-end module fpm_cmd_install
+    write(unit, fmt) "Install configuration"
+    write(unit, fmt) " - library install", &
+      & trim(merge("enabled ", "disabled", self%library))
+
+  end subroutine info
+
+end module fpm_manifest_install
