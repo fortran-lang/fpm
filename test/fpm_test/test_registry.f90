f7cf187fb858d3454f82ed4f87304065b28cbc06
minhqdao <asiancorporator@yahoo.de>
2022-12-25 23:13:46 +0100

    Add test for no config file

---
+++ test/fpm_test/test_registry.f90
@@ -0,0 +1,37 @@
+module test_registry
+   use testsuite, only: new_unittest, unittest_t, error_t, test_failed
+   use fpm_command_line, only: fpm_registry_settings
+   use fpm_registry, only: get_registry
+   implicit none
+   private
+   public collect_registry
+
+contains
+
+   !> Collect unit tests.
+   subroutine collect_registry(tests)
+
+      !> Unit tests to collect.
+      type(unittest_t), allocatable, intent(out) :: tests(:)
+
+      tests = [ &
+      & new_unittest('no-file', no_file) &
+      ]
+
+   end subroutine collect_registry
+
+   subroutine no_file(error)
+
+      type(error_t), allocatable, intent(out) :: error
+
+      type(fpm_registry_settings), allocatable :: registry_settings
+
+      call get_registry(registry_settings)
+
+      if (allocated(registry_settings)) then
+         call test_failed(error, 'registry_settings should not be allocated without config file')
+      end if
+
+   end subroutine no_file
+
+end module test_registry
