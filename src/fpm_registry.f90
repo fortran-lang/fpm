4684acfaf03be1a58bac1f62031cc06a43134333
minhqdao <asiancorporator@yahoo.de>
2022-12-26 18:32:26 +0100

    Unify logic

--- src/fpm_registry.f90
+++ src/fpm_registry.f90
@@ -8,25 +8,33 @@ module fpm_registry
 contains
    !> Obtain registry settings and register local or custom registry if such was specified
    !> in the global config file.
-   subroutine get_registry(reg_settings, path_to_config_file)
+   subroutine get_registry(reg_settings, custom_path_to_config_file)
       !> Registry settings to be obtained.
       type(fpm_registry_settings), allocatable, intent(out) :: reg_settings
       !> Custom path to the config file.
-      character(len=*), optional, intent(in) :: path_to_config_file
+      character(len=*), optional, intent(in) :: custom_path_to_config_file
       !> System-dependent default path to the config file.
       character(len=:), allocatable :: default_path_to_config_file
+      !> Final path to the config file.
+      character(len=:), allocatable :: path_to_config_file
 
-      if (present(path_to_config_file)) then
-         if (exists(path_to_config_file)) then
-            allocate (reg_settings)
-            reg_settings%working_dir = path_to_config_file
+      ! Use custom path to the config file if it was specified.
+      if (present(custom_path_to_config_file)) then
+         if (exists(custom_path_to_config_file)) then
+            path_to_config_file = custom_path_to_config_file
          end if
       else
+         ! Use default path to the config file if it wasn't manually set and exists.
          default_path_to_config_file = join_path(get_local_prefix(), 'fpm', 'config.toml')
          if (exists(default_path_to_config_file)) then
-            allocate (reg_settings)
-            reg_settings%working_dir = default_path_to_config_file
+            path_to_config_file = default_path_to_config_file
          end if
+      end if
+
+      ! Obtain registry settings from config file if it was found.
+      if (allocated(path_to_config_file)) then
+         allocate (reg_settings)
+         reg_settings%working_dir = path_to_config_file
       end if
 
    end subroutine get_registry
