281e6a472f2c259abbf556b81d8d25aaec319cc8
minhqdao <asiancorporator@yahoo.de>
2023-01-02 02:16:02 +0100

    Parse path and url registry settings and add tests

--- src/fpm_settings.f90
+++ src/fpm_settings.f90
@@ -4,6 +4,8 @@ module fpm_settings
    use fpm_filesystem, only: exists, join_path, get_local_prefix
    use fpm_environment, only: os_is_unix
    use fpm_error, only: error_t, fatal_error
+   use fpm_toml, only: toml_table, toml_error, toml_stat, get_value
+   use tomlf, only: toml_load
    implicit none
    private
    public get_global_settings
@@ -13,12 +15,16 @@ contains
    subroutine get_global_settings(global_settings, error, custom_path_to_config_file)
       !> Global settings to be obtained.
       type(fpm_global_settings), allocatable, intent(out) :: global_settings
-      !> Error handling.
+      !> Error reading config file.
       type(error_t), allocatable, intent(out) :: error
       !> Custom path to the config file.
       character(len=*), optional, intent(in) :: custom_path_to_config_file
       !> Final path to the config file.
       character(len=:), allocatable :: path_to_config_file
+      !> TOML table to be filled with global config settings.
+      type(toml_table), allocatable :: table
+      !> Error parsing to TOML table.
+      type(toml_error), allocatable :: parse_error
 
       ! Use custom path to the config file if it was specified.
       if (present(custom_path_to_config_file)) then
@@ -44,6 +50,73 @@ contains
       allocate (global_settings)
       global_settings%path = path_to_config_file
 
+      ! Load into TOML table.
+      call toml_load(table, path_to_config_file, error=parse_error)
+
+      if (allocated(parse_error)) then
+         allocate (error)
+         call move_alloc(parse_error%message, error%message)
+         return
+      end if
+
+      call get_registry_settings(global_settings, table, error)
+
    end subroutine get_global_settings
+
+   subroutine get_registry_settings(global_settings, table, error)
+      type(fpm_global_settings), intent(inout) :: global_settings
+      type(toml_table), intent(inout) :: table
+      type(error_t), allocatable, intent(out) :: error
+      type(toml_table), pointer :: child
+      character(:), allocatable :: path, url
+      integer :: stat
+
+      call get_value(table, 'registry', child, requested=.false., stat=stat)
+
+      if (stat /= toml_stat%success) then
+         call fatal_error(error, 'Error reading registry from config file "'// &
+                          global_settings%path//'"')
+         return
+      end if
+
+      ! Quietly return if no registry table was found.
+      if (.not. associated(child)) return
+
+      allocate (global_settings%registry_settings)
+
+      call get_value(child, 'path', path, stat=stat)
+
+      if (stat /= toml_stat%success) then
+         call fatal_error(error, 'Error parsing path to registry: "'//path//'"')
+         return
+      end if
+
+      if (allocated(path)) then
+         if (.not. exists(path)) then
+            call fatal_error(error, "Path to registry doesn't exist:"//'"'//path//'"')
+            return
+         end if
+
+         global_settings%registry_settings%path = path
+      end if
+
+      call get_value(child, 'url', url, stat=stat)
+
+      if (stat /= toml_stat%success) then
+         call fatal_error(error, 'Error parsing url to registry: "'//url//'"')
+         return
+      end if
+
+      if (allocated(url)) then
+         ! Throw error when both path and url were provided.
+         if (allocated(path)) then
+            call fatal_error(error, 'Do not provide both path and url to registry')
+            return
+         end if
+
+         global_settings%registry_settings%url = url
+      end if
+
+   end subroutine get_registry_settings
 
 end module fpm_settings
--- test/fpm_test/test_settings.f90
+++ test/fpm_test/test_settings.f90
@@ -4,6 +4,7 @@ module test_settings
    use fpm_settings, only: get_global_settings
    use fpm_filesystem, only: is_dir, join_path, mkdir, filewrite, os_delete_dir, exists
    use fpm_environment, only: os_is_unix
+   use fpm_toml, only: new_table
 
    implicit none
    private
@@ -22,10 +23,19 @@ contains
 
       tests = [ &
       & new_unittest('no-file', no_file, should_fail=.true.), &
-      & new_unittest('empty-file', empty_file) &
+      & new_unittest('empty-file', empty_file), &
+      & new_unittest('empty-registry-table', empty_registry_table), &
+      & new_unittest('has-non-existent-path-registry', has_non_existent_path_registry, should_fail=.true.), &
+      & new_unittest('has-existent-path-registry', has_existent_path_registry), &
+      & new_unittest('has-url-registry', has_url_registry), &
+      & new_unittest('has-both-path-and-url-registry', has_both_path_and_url_registry, should_fail=.true.) &
       ]
 
    end subroutine collect_settings
+
+   subroutine delete_tmp_folder
+      if (is_dir(tmp_folder)) call os_delete_dir(os_is_unix(), tmp_folder)
+   end
 
    !> Throw error when custom path to config file was entered but none exists.
    subroutine no_file(error)
@@ -33,8 +43,7 @@ contains
       type(error_t), allocatable, intent(out) :: error
       type(fpm_global_settings), allocatable :: global_settings
 
-      if (is_dir(tmp_folder)) call os_delete_dir(os_is_unix(), tmp_folder)
-
+      call delete_tmp_folder()
       call get_global_settings(global_settings, error, join_path(tmp_folder, config_file_name))
 
    end subroutine no_file
@@ -46,8 +55,7 @@ contains
       type(fpm_global_settings), allocatable :: global_settings
       character(len=:), allocatable :: path_to_config_file
 
-      if (is_dir(tmp_folder)) call os_delete_dir(os_is_unix(), tmp_folder)
-
+      call delete_tmp_folder()
       call mkdir(tmp_folder)
 
       path_to_config_file = join_path(tmp_folder, config_file_name)
@@ -67,6 +75,123 @@ contains
          return
       end if
 
+      if (allocated(global_settings%registry_settings)) then
+         call test_failed(error, 'global_settings%registry_settings should not be allocated')
+         return
+      end if
+
    end subroutine empty_file
+
+   subroutine empty_registry_table(error)
+      type(error_t), allocatable, intent(out) :: error
+      type(fpm_global_settings), allocatable :: global_settings
+      character(len=:), allocatable :: path_to_config_file
+
+      call delete_tmp_folder()
+      call mkdir(tmp_folder)
+
+      path_to_config_file = join_path(tmp_folder, config_file_name)
+      call filewrite(path_to_config_file, ['[registry]'])
+
+      call get_global_settings(global_settings, error, path_to_config_file)
+
+      call os_delete_dir(os_is_unix(), tmp_folder)
+
+      if (.not. allocated(global_settings%registry_settings)) then
+         call test_failed(error, 'global_settings%registry_settings not allocated')
+         return
+      end if
+
+      if (allocated(global_settings%registry_settings%path)) then
+         call test_failed(error, "Path shouldn't be allocated")
+         return
+      end if
+
+      if (allocated(global_settings%registry_settings%url)) then
+         call test_failed(error, "Url shouldn't be allocated")
+         return
+      end if
+
+   end subroutine
+
+   subroutine has_non_existent_path_registry(error)
+      type(error_t), allocatable, intent(out) :: error
+      type(fpm_global_settings), allocatable :: global_settings
+      character(len=:), allocatable :: path_to_config_file
+
+      call delete_tmp_folder()
+      call mkdir(tmp_folder)
+
+      path_to_config_file = join_path(tmp_folder, config_file_name)
+      call filewrite(path_to_config_file, ['[registry]', 'path="abc"'])
+      call get_global_settings(global_settings, error, path_to_config_file)
+      call os_delete_dir(os_is_unix(), tmp_folder)
+   end subroutine
+
+   subroutine has_existent_path_registry(error)
+      type(error_t), allocatable, intent(out) :: error
+      type(fpm_global_settings), allocatable :: global_settings
+      character(len=:), allocatable :: path_to_config_file
+
+      call delete_tmp_folder()
+      call mkdir(tmp_folder)
+
+      path_to_config_file = join_path(tmp_folder, config_file_name)
+      call filewrite(path_to_config_file, ['[registry]', 'path="tmp"'])
+
+      call get_global_settings(global_settings, error, path_to_config_file)
+
+      call os_delete_dir(os_is_unix(), tmp_folder)
+
+      if (.not. allocated(global_settings%registry_settings%path)) then
+         call test_failed(error, 'Path not allocated')
+         return
+      end if
+
+      if (allocated(global_settings%registry_settings%url)) then
+         call test_failed(error, "Url shouldn't be allocated")
+         return
+      end if
+   end subroutine
+
+   subroutine has_url_registry(error)
+      type(error_t), allocatable, intent(out) :: error
+      type(fpm_global_settings), allocatable :: global_settings
+      character(len=:), allocatable :: path_to_config_file
+
+      call delete_tmp_folder()
+      call mkdir(tmp_folder)
+
+      path_to_config_file = join_path(tmp_folder, config_file_name)
+      call filewrite(path_to_config_file, ['[registry]', 'url="http"'])
+
+      call get_global_settings(global_settings, error, path_to_config_file)
+
+      call os_delete_dir(os_is_unix(), tmp_folder)
+
+      if (allocated(global_settings%registry_settings%path)) then
+         call test_failed(error, "Path shouldn't be allocated")
+         return
+      end if
+
+      if (.not. allocated(global_settings%registry_settings%url)) then
+         call test_failed(error, "Url not allocated")
+         return
+      end if
+   end subroutine
+
+   subroutine has_both_path_and_url_registry(error)
+      type(error_t), allocatable, intent(out) :: error
+      type(fpm_global_settings), allocatable :: global_settings
+      character(len=:), allocatable :: path_to_config_file
+
+      call delete_tmp_folder()
+      call mkdir(tmp_folder)
+
+      path_to_config_file = join_path(tmp_folder, config_file_name)
+      call filewrite(path_to_config_file, ['[registry]', 'path="tmp"', 'url="http"'])
+      call get_global_settings(global_settings, error, path_to_config_file)
+      call os_delete_dir(os_is_unix(), tmp_folder)
+   end subroutine
 
 end module test_settings
