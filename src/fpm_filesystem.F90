9c9de7f97f2834e7f4d7ca327c24aa9fd1861359
minhqdao <asiancorporator@yahoo.de>
2023-01-06 20:37:05 +0100

    Fix test

--- src/fpm_filesystem.F90
+++ src/fpm_filesystem.F90
@@ -1007,6 +1007,11 @@ end function get_local_prefix
       if (is_unix_os) then
          is_absolute_path = path(1:1) == '/' .or. path(1:1) == '~'
       else
+         if (len(path) < 2) then
+            is_absolute_path = .false.
+            return
+         end if
+
          is_absolute_path = index(letters, path(1:1)) /= 0 .and. path(2:2) == ':'
       end if
 
