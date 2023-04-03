ce02cbea2dc84474ec64d6218d1fa383c5f126ab
Federico Perini <federico.perini@gmail.com>
2023-04-03 15:52:38 +0200

    write MPI Fortran test program

---
+++ example_packages/metapackage_mpi/app/main.f90
@@ -0,0 +1,30 @@
+program with_mpi
+ 
+  include 'mpif.h'
+
+  integer, parameter :: INIT_ERROR = 1
+  integer, parameter :: RANK_ERROR = 2
+
+  integer :: ierror,ncpus,cpuid
+
+  ! Initialize MPI argument
+  call MPI_INIT(ierror);  
+  if (ierror/=0) stop INIT_ERROR
+
+  ! Get number of processes and current rank 
+  call MPI_Comm_size(MPI_COMM_WORLD, ncpus, ierror)
+  if (ierror/=0) stop RANK_ERROR
+
+  call MPI_Comm_rank(MPI_COMM_WORLD, cpuid, ierror) 
+  if (ierror/=0) stop RANK_ERROR
+
+  print "('Hello, mpi world from rank ',i0,' of ',i0,'!')", cpuid+1,ncpu
+
+  ! Finalize MPI environment.
+  call MPI_FINALIZE(ierror)
+  if (ierror/=0) stop INIT_ERROR
+  
+  stop 0
+
+end program with_mpi 
+
