0e231b508617fbf302df15d7a3e74f70c684154c
Federico Perini <federico.perini@gmail.com>
2023-04-11 16:46:57 +0200

    display build output

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -217,6 +217,9 @@ test $EXIT_CODE -eq 0
 # Build again, should update nothing
 "$fpm" build --verbose > build.log
 if [[ -n "$(grep Update build.log)" ]]; then
+  
+  cat build.log;
+
   echo "Some dependencies were updated that should not be";
   exit 1;
 fi
@@ -224,6 +227,9 @@ fi
 # Request update, should update oth
 "$fpm" update --clean --verbose > update.log
 if [[ -z "$(grep Update update.log)" ]]; then
+
+  cat update.log;
+
   echo "No updated dependencies after 'fpm update --clean'";
   exit 1;
 fi
