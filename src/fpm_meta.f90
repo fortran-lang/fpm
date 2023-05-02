59c5dbde7d3d1b56eb449400db0b7c7704ecf9c8
Federico Perini <federico.perini@gmail.com>
2023-05-02 21:38:46 +0200

    start-group

--- src/fpm_meta.f90
+++ src/fpm_meta.f90
@@ -973,7 +973,7 @@ subroutine init_mpi_from_wrappers(this,compiler,mpilib,fort_wrapper,c_wrapper,cx
 
     ! Request to use libs in arbitrary order
     if (this%has_link_flags .and. compiler%is_gnu() .and. os_is_unix() .and. get_os_type()/=OS_MACOS) then
-        this%link_flags = string_t(' -Wl,--as-needed '//this%link_flags%s)
+        this%link_flags = string_t(' -Wl,--start-group '//this%link_flags%s)
     end if
 
 
