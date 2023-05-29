f415aa16f6ac5aad9446f98b3b7e1aebb6d381c8
minhqdao <43783196+minhqdao@users.noreply.github.com>
2023-05-29 00:20:36 +0000

    Deploying to gh-pages from  @ 757e0033d6a6ad053cdd6ff99397bd810fb19061 🚀

--- src/publish.f90
+++ src/publish.f90
@@ -30,7 +30,7 @@ contains
     type(fpm_model_t) :: model
     type(error_t), allocatable :: error
     type(version_t), allocatable :: version
-    type(string_t), allocatable :: form_data(:)
+    type(string_t), allocatable :: upload_data(:)
     character(len=:), allocatable :: tmp_file
     type(downloader_t) :: downloader
     integer :: i
@@ -44,47 +44,54 @@ contains
       print *, version%s(); return
     end if
 
-    ! Build model to obtain dependency tree.
-    call build_model(model, settings%fpm_build_settings, package, error)
-    if (allocated(error)) call fpm_stop(1, '*cmd_build* Model error: '//error%message)
-
     !> Checks before uploading the package.
     if (.not. allocated(package%license)) call fpm_stop(1, 'No license specified in fpm.toml.')
+    if (.not. package%build%module_naming) call fpm_stop(1, 'The package does not meet the module naming requirements. '// &
+      & 'Please set "module_naming = true" in fpm.toml [build] or specify a custom module prefix.')
     if (.not. allocated(version)) call fpm_stop(1, 'No version specified in fpm.toml.')
     if (version%s() == '0') call fpm_stop(1, 'Invalid version: "'//version%s()//'".')
     if (.not. exists('fpm.toml')) call fpm_stop(1, "Cannot find 'fpm.toml' file. Are you in the project root?")
 
+    ! Build model to obtain dependency tree.
+    call build_model(model, settings%fpm_build_settings, package, error)
+    if (allocated(error)) call fpm_stop(1, '*cmd_build* Model error: '//error%message)
+
     ! Check if package contains git dependencies. Only publish packages without git dependencies.
     do i = 1, model%deps%ndep
       if (allocated(model%deps%dep(i)%git)) then
-        call fpm_stop(1, "Do not publish packages containing git dependencies. '"//model%deps%dep(i)%name//"' is a git dependency.")
+        call fpm_stop(1, 'Do not publish packages containing git dependencies. '// &
+        & "Please upload '"//model%deps%dep(i)%name//"' to the registry first.")
       end if
     end do
 
-    form_data = [ &
+    upload_data = [ &
       string_t('package_name="'//package%name//'"'), &
       string_t('package_license="'//package%license//'"'), &
       string_t('package_version="'//version%s()//'"') &
       & ]
 
-    if (allocated(settings%token)) form_data = [form_data, string_t('upload_token="'//settings%token//'"')]
+    if (allocated(settings%token)) upload_data = [upload_data, string_t('upload_token="'//settings%token//'"')]
 
     tmp_file = get_temp_filename()
     call git_archive('.', tmp_file, error)
     if (allocated(error)) call fpm_stop(1, '*cmd_publish* Pack error: '//error%message)
-    form_data = [form_data, string_t('tarball=@"'//tmp_file//'"')]
+    upload_data = [upload_data, string_t('tarball=@"'//tmp_file//'"')]
 
-    if (settings%show_form_data) then
-      do i = 1, size(form_data)
-        print *, form_data(i)%s
+    if (settings%show_upload_data) then
+      do i = 1, size(upload_data)
+        print *, upload_data(i)%s
       end do
       return
     end if
 
     ! Make sure a token is provided for publishing.
-    if (.not. allocated(settings%token)) call fpm_stop(1, 'No token provided.')
+    if (allocated(settings%token)) then
+      if (settings%token == '') call fpm_stop(1, 'No token provided.')
+    else
+      call fpm_stop(1, 'No token provided.')
+    end if
 
-    call downloader%upload_form(official_registry_base_url//'/packages', form_data, error)
+    call downloader%upload_form(official_registry_base_url//'/packages', upload_data, error)
     if (allocated(error)) call fpm_stop(1, '*cmd_publish* Upload error: '//error%message)
   end
 end
