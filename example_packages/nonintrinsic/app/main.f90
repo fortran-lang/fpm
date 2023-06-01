89f2004d0c24fd1fed45023d69e227064bcb8917
Federico Perini <federico.perini@gmail.com>
2023-06-01 05:53:31 -0500

    add example package

---
+++ example_packages/nonintrinsic/app/main.f90
@@ -0,0 +1,6 @@
+program test_nonintr
+    use, non_intrinsic :: iso_fortran_env
+
+    ! ijk=0 can be read
+    stop ijk
+end program test_nonintr
---
+++ example_packages/nonintrinsic/src/iso_fortran_env.f90
@@ -0,0 +1,4 @@
+module iso_fortran_env
+    implicit none
+    integer, parameter :: ijk = 0
+end module iso_fortran_env    
