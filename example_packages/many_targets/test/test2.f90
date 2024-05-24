00046281470b112c545db55a47383c9eb83c85af
Federico Perini <federico.perini@gmail.com>
2024-05-24 20:04:20 +0200

    add test package

---
+++ example_packages/many_targets/test/test2.f90
@@ -0,0 +1,6 @@
+program test2
+    use file_mod 
+    implicit none
+    call print_file("test",2)
+    stop 0
+end program test2
---
+++ example_packages/many_targets/test/test3.f90
@@ -0,0 +1,6 @@
+program test3
+    use file_mod 
+    implicit none
+    call print_file("test",3)
+    stop 0
+end program test3
