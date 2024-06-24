f3fca0859bf112472c449a602394f9780e989904
Federico Perini <federico.perini@gmail.com>
2024-06-24 07:12:45 -0500

    Update fpm_os.c

--- src/fpm_os.c
+++ src/fpm_os.c
@@ -21,7 +21,7 @@ char* c_realpath(char* path, char* resolved_path, int maxLength) {
 /// @param overwrite: flag to determine whether an old value should be overwritten
 /// @return success flag, 0 on successful execution
 int c_setenv(const char *envname, const char *envval, int overwrite) {
-#ifndef _WIN32    
+#ifndef _WIN32
    return setenv(envname, envval, overwrite);
 #else
    int errcode = 0;
@@ -30,21 +30,24 @@ int c_setenv(const char *envname, const char *envval, int overwrite) {
        errcode = getenv_s(&envsize, NULL, 0, envname);
        if (errcode || envsize) return errcode;
    }
-   return _putenv_s(envname, envval);    
+   return _putenv_s(envname, envval);
 #endif
-} 
+}
 
 /// @brief Delete environment variable using the C standard library
 /// @param envname: points to a string containing the name of an environment variable.
 /// @return success flag, 0 on successful execution
 int c_unsetenv(const char *envname) {
-#ifndef _WIN32    
+#ifndef _WIN32
    return unsetenv(envname);
 #else
+   char* str = malloc(10*sizeof(char));
+   str = '\0';
    // _putenv_s returns a non-zero code when deleting a variable
-   int errcode = _putenv_s(envname,NULL); 
-   return 0;    
-#endif   
-} 
+   int errcode = _putenv_s(envname,str);
+   free(str);
+   return 0;
+#endif
+}
 
 
