d198042d4e8fa7e1840e53f3ac55e1a9358f9d4a
Christopher Albert <albert@tugraz.at>
2025-04-08 21:34:03 +0200

    MKL compiler flag for Intel

--- src/metapackage/fpm_meta_blas.f90
+++ src/metapackage/fpm_meta_blas.f90
@@ -23,26 +23,34 @@ contains
         integer :: i
         character(len=:), allocatable :: include_flag, libdir
         character(*), parameter :: candidates(*) = &
-                     [character(20) :: 'mkl-dynamic-lp64-tbb', 'openblas', 'blas']
+                                   [character(20) :: 'mkl-dynamic-lp64-tbb', 'openblas', 'blas']
 
         include_flag = get_include_flag(compiler, "")
 
         !> Cleanup
         call destroy(this)
-        allocate (this % link_libs(0), this % incl_dirs(0), this % external_modules(0))
-        this % link_flags = string_t("")
-        this % flags = string_t("")
-        this % has_external_modules = .false.
+        allocate (this%link_libs(0), this%incl_dirs(0), this%external_modules(0))
+        this%link_flags = string_t("")
+        this%flags = string_t("")
+        this%has_external_modules = .false.
 
-        !> Assert pkg-config is installed
-        if (.not. assert_pkg_config()) then
-            call fatal_error(error, 'blas metapackage requires pkg-config')
+        if (compiler%is_intel()) then
+            this%flags = string_t("-qmkl")
+            this%has_build_flags = .true.
+            this%link_flags = string_t("-qmkl")
+            this%has_link_flags = .true.
             return
         end if
 
-        do i=1,size(candidates)
+        !> Assert pkg-config is installed
+        if (.not. assert_pkg_config()) then
+            call fatal_error(error, 'blas metapackage requires pkg-config to continue lookup')
+            return
+        end if
+
+        do i = 1, size(candidates)
             if (pkgcfg_has_package(trim(candidates(i)))) then
-                call add_pkg_config_compile_options(&
+                call add_pkg_config_compile_options( &
                     this, trim(candidates(i)), include_flag, libdir, error)
                 print *, 'found blas package: ', trim(candidates(i))
                 return
