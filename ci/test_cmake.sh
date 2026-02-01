fd1bc63aefa60a4eb9615d8bd4a51dd078a401ce
Jeff Irwin <jirwin505@gmail.com>
2026-02-01 14:50:27 -0500

    run the example package exe's built by cmake

--- ci/test_cmake.sh
+++ ci/test_cmake.sh
@@ -6,7 +6,8 @@
 #set -exu
 set -u
 
-failures=()
+build_failures=()
+runtime_failures=()
 
 for dir in example_packages/*/ ; do
 
@@ -20,12 +21,30 @@ for dir in example_packages/*/ ; do
 	cmake -B temp_cmake_build -S .
 	cmake --build temp_cmake_build --parallel
 	if [[ $? -ne 0 ]] ; then
-		failures+=("$dir")
-	fi
+		build_failures+=("$dir")
+	else
+		# Find and run executables. Use maxdepth because dependencies like
+		# test-drive have executable hooks not built by us that I don't want to
+		# run
+		exes=$(find temp_cmake_build -maxdepth 1 -type f -executable | grep -v '/CMakeFiles/')
+		#exes=$(find temp_cmake_build -type f -executable | grep -v '/CMakeFiles/')
+		for exe in $exes; do
 
-	# TODO: also find the app exe, run it, and check the return code.  Some of
-	# the macro examples may build successfully but fail to run if macros are
-	# defined incorrectly
+			# TODO
+			[[ "$dir" == "example_packages/metapackage_stdlib_extblas/" ]] && continue
+			[[ "$dir" == "example_packages/preprocess_per_dependency/" ]] && continue
+
+			#[[ "$dir" == "example_packages/dependency_priority/" ]] && continue  # hangs indefinitely
+			[[ "$dir" == "example_packages/fpm_test_exit_code/" ]] && continue  # returns 1 on purpose
+
+			if [[ -x "$exe" ]]; then
+				"$exe"
+				if [[ $? -ne 0 ]]; then
+					runtime_failures+=("$dir : $(basename $exe)")
+				fi
+			fi
+		done
+	fi
 
 	# Cleanup
 	rm CMakeLists.txt
@@ -35,8 +54,30 @@ for dir in example_packages/*/ ; do
 
 done
 
-echo "failures = "
-echo "${failures[@]}"
+echo "================================"
+echo "Test Results Summary"
+echo "================================"
+
+if [[ ${#build_failures[@]} -eq 0 ]]; then
+	echo "Build failures: none"
+else
+	echo "Build failures (${#build_failures[@]}):"
+	printf '  %s\n' "${build_failures[@]}"
+fi
+
+echo ""
+
+if [[ ${#runtime_failures[@]} -eq 0 ]]; then
+	echo "Runtime failures: none"
+else
+	echo "Runtime failures (${#runtime_failures[@]}):"
+	printf '  %s\n' "${runtime_failures[@]}"
+fi
+
+echo "================================"
+
+total_failures=$((${#build_failures[@]} + ${#runtime_failures[@]}))
+exit $total_failures
 
 # TODO: test fpm self-generation too:
 #
